05e2da11e233618b922f61a62e5a5148093a8a8b
root <root@Davi.localdomain>
2009-02-09 19:41:34 -0200

    configuração de upload relizada

--- app/models/directory.rb
+++ app/models/directory.rb
@@ -1,2 +1,11 @@
 class Directory < ActiveRecord::Base
+  has_many :archives
+  validates_presence_of :name
+
+def archive_attributes=(archive_attributes)
+  archive_attributes.each do |attributes|
+    archives.build(attributes)
+  end
+end
+
 end
